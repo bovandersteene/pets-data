4515c0ff01465a2a81c3879a0a7c32269c6f4925
bovandersteene <bo.vandersteene@gmail.com>
2017-06-06 10:36:01 +0200

    exercise 1

---
+++ src/main/java/io/reibo/pets/data/common/Gender.java
@@ -0,0 +1,8 @@
+package io.reibo.pets.data.common;
+
+/**
+ * Created by bovandersteene on 06/06/2017.
+ */
+public enum Gender {
+    FEMALE, MALE
+}
--- src/main/java/io/reibo/pets/data/owner/Owner.java
+++ src/main/java/io/reibo/pets/data/owner/Owner.java
@@ -1,10 +1,65 @@
 package io.reibo.pets.data.owner;
 
+import io.reibo.pets.data.common.Gender;
+
+import java.time.LocalDateTime;
+
 /**
  * Created by bovandersteene on 06/06/2017.
  */
 public class Owner {
-    public Long id;
-    public String firstname;
-    public String lastname;
+    private Long id;
+    private String firstname;
+    private String lastname;
+    private LocalDateTime creationDate;
+    private LocalDateTime lastModifiedDate;
+    private Gender gender;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getFirstname() {
+        return firstname;
+    }
+
+    public void setFirstname(String firstname) {
+        this.firstname = firstname;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+
+    public LocalDateTime getCreationDate() {
+        return creationDate;
+    }
+
+    public void setCreationDate(LocalDateTime creationDate) {
+        this.creationDate = creationDate;
+    }
+
+    public LocalDateTime getLastModifiedDate() {
+        return lastModifiedDate;
+    }
+
+    public void setLastModifiedDate(LocalDateTime lastModifiedDate) {
+        this.lastModifiedDate = lastModifiedDate;
+    }
+
+    public Gender getGender() {
+        return gender;
+    }
+
+    public void setGender(Gender gender) {
+        this.gender = gender;
+    }
 }
