3c749dcfa50bf81067c7e1d7013cdf83db320bc2
bovandersteene <bo.vandersteene@gmail.com>
2017-06-06 10:31:00 +0200

    initial commit

---
+++ src/main/java/io/reibo/pets/data/owner/Owner.java
@@ -0,0 +1,10 @@
+package io.reibo.pets.data.owner;
+
+/**
+ * Created by bovandersteene on 06/06/2017.
+ */
+public class Owner {
+    public Long id;
+    public String firstname;
+    public String lastname;
+}
---
+++ src/main/java/io/reibo/pets/data/owner/OwnerRepository.java
@@ -0,0 +1,7 @@
+package io.reibo.pets.data.owner;
+
+/**
+ * Created by bovandersteene on 06/06/2017.
+ */
+public class OwnerRepository {
+}
